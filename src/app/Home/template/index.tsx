9115ef044255b763157aee0158c8d7839e4d94c3
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-04-29 22:29:24 +0500

    Initial commit

---
+++ src/app/Home/template/index.tsx
@@ -0,0 +1,10 @@
+import { Typography } from "@mui/material";
+import React from "react";
+
+type Props = {};
+
+const HomePage = (props: Props) => {
+  return <Typography>home</Typography>;
+};
+
+export default HomePage;
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+import HomePage from "./Home/template";
+
+export default function Home() {
+  return <HomePage />;
+}
