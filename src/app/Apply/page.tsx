24f247ee1ba17b1eace2707a6c8f44f3ab6c3b9a
areeb-gsoftconsulting <syedareeb@gsoftconsulting.com>
2024-06-16 13:10:04 +0500

    layout

--- src/app/Apply/page.tsx
+++ src/app/Apply/page.tsx
@@ -1,5 +1,14 @@
 import ApplyPage from "./template/index";
+import Head from "next/head";
 
 export default function Apply() {
-  return <ApplyPage />;
+  return (
+    <>
+      <Head>
+        <title>Apply Now - ESK IT</title>
+        <meta name="description" content="Learn more about us on this page" />
+      </Head>
+      <ApplyPage />;
+    </>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,8 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ESK IT",
-  description: "ESK IT",
+  description:
+    "ESK IT - No IT background or college degree required, change your career into: You will be able to apply for the following jobs after training.",
 };
 
 export default function RootLayout({
